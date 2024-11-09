0ef86c48dba22a96ab2849391dd3d8ff87f61e37
rayansatti <rayansatti013@gmail.com>
2024-11-09 21:42:33 +1100

    finishes the knight moves function

--- knights.js
+++ knights.js
@@ -14,5 +14,20 @@ function knightMoves(start, target){
 
     while(queue.length > 0){
         const [currentPos, path] = queue.shift();
+
+        if(currentPos[0] === target[0] && currentPos[1] === target[1]){
+            console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+            path.forEach(pos => console.log(pos));
+            return path;
+        }
+    }
+
+    for(const move of moves){
+        const newPosition = [currentPos[0] + move[0], currentPos[1] + move[1]];
+
+        if(isWithinBoard(newPosition) && !visited.has(newPosition.toString())){
+            visited.add(newPosition.toString());
+            queue.push([newPosition, path.concat([newPosition])]);
+        }
     }
 }
